22200ad9bd9c4985859d1e3952d55b40d1217976
Matthew Ashton <mashton237@fullsail.com>
2013-05-02 20:25:40 -0400

    Added Array Adding

--- rename-me.js
+++ rename-me.js
@@ -39,21 +39,33 @@ var myLibrary = function(){
         return num.toFixed(val);
     }
     
-    
-    
+    // Adding numbers in an array
+    var arrayAdding = function (){
+    var total = 0;
+    for(var i = 0; i < array.length; i++){
+    var val = parseInt(array[i]);
+        if(!isNaN(val)){
+        total += val;
+        }
+    }
+ return total;
+}     
     return {
         "stringIntoNumber": stringIntoNumber,
         "checkEmailAddress": checkEmailAddress,
         "checkPhoneNumber": checkPhoneNumber,
         "titleString": titleString,
-        "roundNumber": roundNumber
+        "roundNumber": roundNumber,
+        "arrayAdding": arrayAdding
     }
 }
 
 var newLib = new myLibrary();
+var array = [3, 4, "yup", 5]
 
 console.log( + newLib.stringIntoNumber("42"));
 console.log("My Name is " + newLib.titleString("matt ashton"));
 console.log("Is 239-691-3090 a phone number? "+ newLib.checkPhoneNumber("239-691-3090"));
 console.log("Is mashton237@fullsail.com an email address? " + newLib.checkEmailAddress("mashton237@fullsail.com"));
 console.log( + newLib.roundNumber(3.145653, 2));
+console.log( + newLib.arrayAdding([array]));
